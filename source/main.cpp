558f2c4b95854753df46eb8172f8051ad5575d77
Atticus Templeton <atempleton23@my.whitworth.edu>
2022-04-06 13:53:21 -0700

    added some to adatafile

--- source/main.cpp
+++ source/main.cpp
@@ -252,9 +252,13 @@ void aDataFile(processtable& PT){
     long prioAccum[queueCount+1];
     long prioCount[queueCount+1];
     long avgAccum, avgArriv, avgPrio = 0;
-    int processes = PT.oldSize();
+    int processes = PT.oldSize() + PT.size();
+    float throughPut = static_cast<float>(processes) / static_cast<float>(timer);
+    
+    
 
     // TODO: FINALIZE ANALYSIS METHODS AND IMPLEMENT
+    //int timeTaken = PT.closure - PT.arrival
     cout << "TODO" << endl;
 }
 
@@ -386,6 +390,9 @@ int main(){
     for(int i = 0; i < NUMCORES; i++) {
         processors[i].join();
     }
+
+    //throughput = processCount / timer
+    //cout << "Throughput: "  << processCount / timer << endl;
 
     // Do analysis stuff via PCB information
     analyze(PT);
